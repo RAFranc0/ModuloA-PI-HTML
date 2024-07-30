b39e5a3dd7947113581920080a8e8a987428323c
Raul <raul.franco266@gmail.com>
2024-07-29 21:18:49 -0300

    Musica em Ação - HTML/CSS/JS

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-fetch('Shared/cabecalho.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById('cabecalho').innerHTML = data;
-    })
-    .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('Shared/cabecalho.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('cabecalho').innerHTML = data;
+        })
+        .catch(error => console.error('Erro ao carregar o cabeçalho:', error));
 
-document.getElementById('formContato').addEventListener('submit', function (event) {
-    event.preventDefault(); 
-    window.location.href = "ConfirmacaoEnvio.html";
+    var formContato = document.getElementById('formContato');
+    if (formContato) {
+        formContato.addEventListener('submit', function (event) {
+            event.preventDefault(); 
+            window.location.href = "ConfirmacaoEnvio.html";
+        });
+    }
+
+    var btnVoltar = document.getElementById('btnVoltar');
+    if (btnVoltar) {
+        btnVoltar.addEventListener('click', function () {
+            window.location.href = "Index.html";
+        });
+    }
 });
